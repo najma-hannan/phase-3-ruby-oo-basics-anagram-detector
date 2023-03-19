391a802e8f078baf2ed8a2475be5a5822dab9ec5
Hannan <najma.mahat@student.moringaschool.com>
2023-03-19 15:32:44 +0300

    Completed lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        matched = []
+        arr.each do |item|
+            if item.split("").sort == @word.split("").sort
+                matched.push(item)
+            end
+        end
+        return matched
+    end
+end
